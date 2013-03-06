f79503cc7f5c72ea57b451452d401f29515def2d
Erik <erik.zetterqvist@ssab.com>
2013-03-06 12:54:59 +0100

    Initial commit

---
+++ NlogWPFControl/NlogViewer.xaml.cs
@@ -0,0 +1,60 @@
+﻿using NLog.Targets;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NlogWPFControl
+{
+    /// <summary>
+    /// Interaction logic for NlogViewer.xaml
+    /// </summary>
+    public partial class NlogViewer : UserControl
+    {
+        public ObservableCollection<LogEventViewModel> LogEntries { get; private set; }
+        public bool IsTargetConfigured { get; private set; }
+
+
+        public NlogViewer()
+        {
+            IsTargetConfigured = false;
+            LogEntries = new ObservableCollection<LogEventViewModel>();
+
+            InitializeComponent();
+
+            if (!DesignerProperties.GetIsInDesignMode(this))
+            {
+                foreach (WPFControlTarget target in NLog.LogManager.Configuration.AllTargets.Where(t => t is WPFControlTarget).Cast<WPFControlTarget>())
+                {
+                    IsTargetConfigured = true;
+                    target.LogReceived += LogReceived;
+                }
+            }
+        }
+
+        protected void LogReceived(NLog.Common.AsyncLogEventInfo log)
+        {
+            LogEventViewModel vm = new LogEventViewModel(log.LogEvent);
+
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                if (LogEntries.Count >= 50)
+                    LogEntries.RemoveAt(0);
+                
+                LogEntries.Add(vm);
+            }));
+        }
+    }
+}
---
+++ Sample/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Sample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Task _logTask;
+        CancellationTokenSource _cancelLogTask;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            Logger log = NLog.LogManager.GetLogger("button");
+
+            LogLevel level = LogLevel.Trace;
+            if (sender.Equals(btnDebug)) level = LogLevel.Debug;
+            if (sender.Equals(btnWarning)) level = LogLevel.Warn;
+            if (sender.Equals(btnError)) level = LogLevel.Error;
+
+            log.Log(level, tbLogText.Text);
+        }
+
+        private void BackgroundSending_Checked(object sender, RoutedEventArgs e)
+        {
+            _cancelLogTask = new CancellationTokenSource();
+            var token = _cancelLogTask.Token;
+            _logTask = new Task(SendLogs, token, token);
+            _logTask.Start();
+        }
+
+        private void BackgroundSending_Unchecked(object sender, RoutedEventArgs e)
+        {
+            if (_cancelLogTask != null)
+                _cancelLogTask.Cancel();
+        }
+
+        private void SendLogs(object obj)
+        {
+            CancellationToken ct = (CancellationToken)obj;
+
+            int counter = 0;
+            Logger log = NLog.LogManager.GetLogger("task");
+
+            log.Debug("Backgroundtask started.");
+
+            while (!ct.WaitHandle.WaitOne(2000))
+            {
+                log.Trace(String.Format("Messageno {0} from backgroudtask.", counter++));
+            }
+
+            log.Debug("Backgroundtask stopped.");
+        }
+
+
+    }
+}
