46b98b2ef0dedbd0432df3d2082c56b78e92c7cd
Erik <erik.zetterqvist@ssab.com>
2013-03-06 13:04:34 +0100

    changed name

---
+++ NlogViewer/LogEventViewModel.cs
@@ -0,0 +1,62 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace NlogViewer
+{
+    public class LogEventViewModel
+    {
+        private NLog.LogEventInfo logEventInfo;
+
+        public LogEventViewModel(NLog.LogEventInfo logEventInfo)
+        {
+            // TODO: Complete member initialization
+            this.logEventInfo = logEventInfo;
+
+            ToolTip = logEventInfo.FormattedMessage;
+            Level = logEventInfo.Level.ToString();
+            FormattedMessage = logEventInfo.FormattedMessage;
+            Exception = logEventInfo.Exception;
+            LoggerName = logEventInfo.LoggerName;
+
+            SetupColors(logEventInfo);
+
+        }
+
+        private void SetupColors(NLog.LogEventInfo logEventInfo)
+        {
+            if (logEventInfo.Level == LogLevel.Warn)
+            {
+                Background = Brushes.Yellow;
+            }
+            else if (logEventInfo.Level == LogLevel.Error)
+            {
+                Background = Brushes.Tomato;
+            }
+            else
+            {
+                Background = Brushes.White;
+            }
+            Foreground = Brushes.Black;
+        }
+
+
+        public string LoggerName { get; private set; }
+        public string Level { get; private set; }
+        public string FormattedMessage { get; private set; }
+        public Exception Exception { get; private set; }
+        public string ToolTip { get; private set; }
+        public SolidColorBrush Background { get; private set; }
+        public SolidColorBrush Foreground { get; private set; }
+        public SolidColorBrush BackgroundMouseOver { get; private set; }
+        public SolidColorBrush ForegroundMouseOver { get; private set; }
+
+
+
+
+    }
+}
