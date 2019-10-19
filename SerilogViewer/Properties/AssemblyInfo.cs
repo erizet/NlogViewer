1f7f8cd151d0cf2059e1e9120d728cfe3a7fe79f
Fabiano Guida <fabiano.guida@outlook.com>
2019-10-19 05:09:58 +0200

    Enabled nuget packaging, fixes #3

--- SerilogViewer/Properties/AssemblyInfo.cs
+++ SerilogViewer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SerilogViewer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("SerilogViewer is a simple WPF-control to show Serilog-logs.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CodeAphex")]
 [assembly: AssemblyProduct("SerilogViewer")]
-[assembly: AssemblyCopyright("Copyright © CodeAphex 2019")]
+[assembly: AssemblyCopyright("Copyright © 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.2.1.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
